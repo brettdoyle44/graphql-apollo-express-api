8d1bcb3dafedfaae2ff8d096ed88523b09ef2c0c
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-09-13 18:25:05 -0400

    Initial Commit
    - Server set up
    - Basic resolvers and typeDefs

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import typeDefs from './typeDefs'
+import resolvers from './resolvers'
+
+const app = express()
+
+const { APP_PORT = 4000, NODE_ENV = 'development' } = process.env
+
+const IN_PROD = NODE_ENV === 'production'
+
+app.disable('x-powered-by')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: !IN_PROD
+})
+
+server.applyMiddleware({ app })
+
+app.listen({ port: APP_PORT }, () =>
+  console.log(`http://localhost:${APP_PORT}${server.graphqlPath}`)
+)
