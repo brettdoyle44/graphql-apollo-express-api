6939db392559b22c25fe6792c2dffcf7aba1ca87
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-09-15 11:41:58 -0400

    Add bcrypt and hash
    - All passwords are now hashed

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { hash } from 'bcryptjs'
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,4 +14,15 @@ const userSchema = new mongoose.Schema(
   }
 )
 
-export default mongoose.model('User', user)
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    try {
+      this.password = await hash(this.password, 10)
+    } catch (err) {
+      next(err)
+    }
+  }
+  next()
+})
+
+export default mongoose.model('User', userSchema)
